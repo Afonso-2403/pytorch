0b184c0919ee64245acdb1f70e28147c3d9747a0
Afonso-2403 <avsluz@gmail.com>
2021-08-16 17:50:57 +0100

    supported AvgPool2d

--- aten/src/ATen/native/cpu/AvgPoolKernel.cpp
+++ aten/src/ATen/native/cpu/AvgPoolKernel.cpp
@@ -366,13 +366,13 @@ void avg_pool2d_kernel_impl(
     c10::optional<int64_t> divisor_override) {
   switch (input.suggest_memory_format()) {
     case at::MemoryFormat::Contiguous: {
-      AT_DISPATCH_FLOATING_TYPES_AND(at::ScalarType::Long, input.scalar_type(), "avg_pool2d", [&] {
+      AT_DISPATCH_FLOATING_TYPES_AND2(at::ScalarType::Long, at::ScalarType::Posit16es2, input.scalar_type(), "avg_pool2d", [&] {
         cpu_avg_pool<scalar_t>(output, input, kW, kH, dW, dH, padW, padH, count_include_pad, divisor_override);
       });
       break;
     }
     case at::MemoryFormat::ChannelsLast: {
-      AT_DISPATCH_FLOATING_TYPES_AND(at::ScalarType::Long, input.scalar_type(), "avg_pool2d_channels_last", [&] {
+      AT_DISPATCH_FLOATING_TYPES_AND2(at::ScalarType::Long, at::ScalarType::Posit16es2, input.scalar_type(), "avg_pool2d_channels_last", [&] {
         cpu_avg_pool_channels_last<scalar_t>(output, input, kW, kH, dW, dH, padW, padH, count_include_pad, divisor_override);
       });
       break;
